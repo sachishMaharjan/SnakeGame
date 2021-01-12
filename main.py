b19dd16f8c0d03993b66e9477fadfa7ec6975efc
Sachish Maharjan <sachish.maharjan12@gmail.com>
2021-01-12 21:55:07 +1100

    Fix:Update main.py using slice

--- main.py
+++ main.py
@@ -39,11 +39,9 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-    # Detect collision with tail.
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # Detect collision with tail.:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
